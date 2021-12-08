b19146c57f04cc9d2dff096d02aea59db51f9db7
Mariam El Baz <60982075+mariambaz@users.noreply.github.com>
2021-12-09 00:56:56 +0200

    Change directory for file reading

--- src/Coordinator.cc
+++ src/Coordinator.cc
@@ -1,9 +1,17 @@
 #include "Coordinator.h"
 
+#include "helpers.h"
+
 Define_Module(Coordinator);
 
 void Coordinator::initialize()
 {
+    vector<string> lines;
+    lines = readFile("E:/Uni Stuff/Senior II Standing/Computer Networks I/Project/Project-2021-Student/SelectiveRepeatProtocol/src/inputs/coordinator.txt");
+    cout << lines.size() << "HELLO";
+    for (int i = 0; i < lines.size(); i++) {
+        cout << lines[i] << endl;
+    }
 }
 
 void Coordinator::handleMessage(cMessage *msg)
--- src/helpers.h
+++ src/helpers.h
@@ -1,2 +1,20 @@
-#include <string.h>
+#include <string>
 #include <fstream>
+#include <vector>
+using namespace std;
+
+vector<string> readFile(string path){
+  ifstream file; 
+  file.open(path);
+  string line;
+  vector<string> output;
+  if (file.is_open()) {
+    while (!file.eof()) {
+      getline(file, line);
+      cout << line;
+      output.push_back(line);
+    }
+  }
+  file.close();
+  return output;
+}
